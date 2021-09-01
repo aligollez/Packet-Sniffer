52626ddb2272bd3299f55b9acd4806c91964fd56
Mary Kroustali <71144000+MaryKroustali@users.noreply.github.com>
2021-09-02 00:20:09 +0300

    Add files via upload

--- pye.py
+++ pye.py
@@ -34,12 +34,19 @@ class unpack:
         header = struct.unpack("!BBHHHBBH4s4s", data)
 
         version = header[0]
+        tos = header[1]
+        length = header[2]
+        id = header[3]
+        offset = header[4]
         ttl = header[5]
         protocol = header[6]
+        checksum = header[7]
         source_ip = socket.inet_ntoa(header[8])
         destination_ip = socket.inet_ntoa(header[9])
 
-        data = {"Version" : version, "Time-To-Live" : ttl, "Protocol" : protocol, "Source IP Address" : source_ip, "Destination IP Address" : destination_ip}
+        data = {"Version" : version, "TOS" : tos, "Total length" : length, "Identification" : id, "Fragment Offset" : offset,
+         "Time-To-Live" : ttl, "Protocol" : protocol, "Header CheckSum" : checksum, "Source IP Address" : source_ip, 
+         "Destination IP Address" : destination_ip}
         return data
 
     # TCP header
@@ -55,6 +62,14 @@ class unpack:
         source_port = header[0]
         destination_port = header[1]
         sequence_number = header[2]
+        ack_number = header[3]
+        offset = header[4]
+        tcp_flag = header[5]
+        window = header[6]
+        checksum = header[7]
+        urgent = header[8]
 
-        data = {"Source Port" : source_port, "Destination Port" : destination_port, "Sequence Number" : sequence_number}
+        data = {"Source Port" : source_port, "Destination Port" : destination_port, "Sequence Number" : sequence_number,
+        "Acknowledge Number" : ack_number, "Offset" : offset, "TCP flag" : tcp_flag, "Window Size" : window, "Checksum" : checksum,
+        "Urgent Pointer" : urgent}
         return data
