5d1e2c5e3b5d228f0443712c4133c5e231509396
Mary Kroustali <71144000+MaryKroustali@users.noreply.github.com>
2021-09-01 23:02:46 +0300

    Add files via upload

--- pye.py
+++ pye.py
@@ -3,6 +3,7 @@ import binascii
 import socket
 
 class unpack:
+    # Constructor
     def __cinit__(self):
         self.data=None
 
@@ -10,11 +11,39 @@ class unpack:
     def eth_header(self, data):
 
         # format - 6s=string type for source/destination MAC addresses - H=unsigned short for ethernet protocol type
-        storeobj = struct.unpack("!6s6sH",data)
+        header = struct.unpack("!6s6sH",data)
 
-        destination_mac=binascii.hexlify(storeobj[0])
-        source_mac=binascii.hexlify(storeobj[1])
-        eth_protocol=storeobj[2]
+        destination_mac = binascii.hexlify(header[0])
+        source_mac = binascii.hexlify(header[1])
+        eth_protocol = header[2]
 
-        data={"Destination Mac":destination_mac, "Source Mac":source_mac, "Protocol":eth_protocol}
+        data = {"Destination Mac" : destination_mac, "Source Mac" : source_mac, "Protocol Type" : eth_protocol}
+        return data
+
+    # IP Header
+    def ip_header(self, data):
+
+        # format - B ttl/protocol - H checksum - 4s=string ips
+        header = struct.unpack("!BBHHHBBH4s4s", data)
+
+        version = header[0]
+        ttl = header[5]
+        protocol = header[6]
+        source_ip = socket.inet_ntoa(header[8])
+        destination_ip = socket.inet_ntoa(header[9])
+
+        data = {"Version" : version, "Time-To-Live" : ttl, "Protocol" : protocol, "Source IP Address" : source_ip, "Destination IP Address" : destination_ip}
+        return data
+
+    # TCP header
+    def tcp_header(self, data):
+
+        # format - H=unsigned short for ports - L=long int seq/ack number - B tcp_flag/offset - H window/checksum/urgent
+        header = struct.unpack("!HHLLBBHHH", data)
+
+        source_port = header[0]
+        destination_port = header[1]
+        sequence_number = header[2]
+
+        data = {"Source Port" : source_port, "Destination Port" : destination_port, "Sequence Number" : sequence_number}
         return data
--- sniffing.py
+++ sniffing.py
@@ -1,22 +1,41 @@
 import socket
-import struct
-import binascii
-import os
 import pye
+
 # run with administrator privilages
 
-s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
-s.bind(("127.0.0.1",0))
-s.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
-s.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
+# - not detecting os info
+
+# Create socket to capture packets
+print("\nCreating socket...")
+sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
+sock.bind(("127.0.0.1",0))
+sock.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
+sock.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
 
 while True:
-    packet = s.recvfrom(65565)
 
-    unpack=pye.unpack()
+    print("\nReceiving packet...")
+    packet = sock.recvfrom(65565) # get data from socket
+    unpack = pye.unpack() # unpuck received data to get headers
 
-    # Ethernet
-    print("\n>>----- Ethernet Header -----")
+    # Ethernet header
+    print("\n>> ----- Ethernet Header -----")
     for i in unpack.eth_header(packet[0][0:14]).items():
         a,b=i
+        if ((a=="Destination Mac") or (a=="Source Mac")):
+            b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]), ord(a[1]), ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5])) # Mac address format
         print("{} : {}".format(a,b))
+
+    # IP header
+    print("\n>> ----- IP Header -----")
+    for i in unpack.ip_header(packet[0][14:34]).items():
+        a,b=i
+        print("{} : {}".format(a,b))
+
+    # TCP header
+    print("\n>> ----- TCP Header -----")
+    for i in unpack.tcp_header(packet[0][14:34]).items():
+        a,b=i
+        print("{} : {}".format(a,b))
+
+
